b3b40bcc80971d5674d11bf4ee74cd9564b580d9
kvs788 <83685332+kvs788@users.noreply.github.com>
2021-09-12 21:43:31 +0530

    Add files via upload

---
+++ my captain task 1.py
@@ -0,0 +1,6 @@
+#finding the area of a circle
+from math import pi
+r=float(input('input the radius of the circle: '))
+A= float(pi*r**2)
+print('the area of the circle with radius '+str(r)+' is: '+  str(A)) 
+
